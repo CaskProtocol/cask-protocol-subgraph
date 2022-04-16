9499becc60d6a80c771b35852f4ecdd785e1cc67
mike <masha256bit@protonmail.com>
2022-04-16 10:07:07 -0700

    add abi importer

---
+++ scripts/update-abis.ts
@@ -0,0 +1,28 @@
+import * as fs from 'fs-extra';
+import * as path from 'path';
+import * as glob from 'glob';
+
+const artifactPath = path.resolve(__dirname, '../../p-cask-contracts/artifacts/contracts/interfaces');
+const outputPath = path.resolve(__dirname, '../abis');
+
+
+const abiJSONfilenames = {
+    'ICaskVault.json': 'CaskVault.json',
+    'ICaskSubscriptions.json': 'CaskSubscriptions.json'
+};
+
+glob(artifactPath + '/**/!(*dbg).json', {}, (err, files) => {
+    if (err) {
+        console.error(err);
+    }
+    for (const filename of files) {
+        const split = filename.split('/');
+        const contractName = split[split.length - 1];
+        const outputName = abiJSONfilenames[contractName];
+        if (outputName) {
+            console.log(`Writing contract ${contractName} to ${outputPath+'/'+outputName}`);
+            const abi = JSON.parse(fs.readFileSync(path.resolve(filename), 'utf8')).abi;
+            fs.writeFileSync(outputPath + '/' + outputName, JSON.stringify(abi, null, 2));
+        }
+    }
+});
