0370f0e119a438089a58a69fe4804b9bda8c95e8
mike <masha256bit@protonmail.com>
2022-05-10 20:36:49 -0700

    polygon mainnet

--- scripts/generate-manifests.ts
+++ scripts/generate-manifests.ts
@@ -13,7 +13,7 @@ const generateManifests = async (): Promise<void> => {
     const template = fs.readFileSync('subgraph.template.yaml').toString();
     Object.entries(networks).forEach(([network, config]) => {
         fs.writeFileSync(
-            `subgraph${network === 'matic' ? '' : `.${network}`}.yaml`,
+            `subgraph.${network}.yaml`,
             Handlebars.compile(template)(config)
         );
     });
